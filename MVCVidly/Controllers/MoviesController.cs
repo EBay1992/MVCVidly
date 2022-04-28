740c6a416d2a846e18c672cf682610da7259d797
Ehsan <ehsanbayranvand@gmail.com>
2022-04-29 02:58:16 +0430

    examine different return and how to get optional parameter form url or query or body

---
+++ MVCVidly/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCVidly.Models;
+
+namespace MVCVidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Id = 1, Name = "Sherek" };
+
+            //return View(movie);
+            //return Content("Hello world"); //return a result as a text 
+            //return NotFound();  // it doens't work! nither
+            //return new EmptyResult(); // a empty page as a response
+            return RedirectToAction("Index", "Home", new {sortby = false, page = 1 }); //firs argument:action, secondone: controller, thrid argument, specify some parameter as a query stirng
+        }
+
+        public IActionResult Edit(int id) //the aps.net authomatically get id from query, param, or body of request and pass to id
+        {
+            return Content($"this is the id your wrote: {id}");
+        }
+
+        public IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if(!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "name";
+            }
+
+            return Content($"pageIndex = {pageIndex}, sortby = {sortBy}");
+        }
+    }
+}
